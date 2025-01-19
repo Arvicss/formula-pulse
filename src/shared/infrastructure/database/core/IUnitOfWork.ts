d707f74b33756dd0bc927f94bb2d704582f58d3c
Arvicss <ramosarvyx@gmail.com>
2025-01-20 03:12:31 +0800

    chore: install dependencies and set up core modules

---
+++ src/shared/infrastructure/database/core/IUnitOfWork.ts
@@ -0,0 +1,6 @@
+export interface IUnitOfWork {
+  startTransaction(): Promise<void>;
+  commitTransaction(): Promise<void>;
+  abortTransaction(): Promise<void>;
+  endTransaction(): Promise<void>;
+}
